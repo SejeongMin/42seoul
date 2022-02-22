b1708be5ccc87edda7c7c48f735e67f077155b9a
Sejeong Min <sj.min621@gmail.com>
2022-02-23 04:23:32 +0900

    cpp01-ex00/

--- cpp-module/cpp-module-00/ex00/megaphone.cpp
+++ cpp-module/cpp-module-00/ex00/megaphone.cpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: semin <semin@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/23 04:20:40 by semin             #+#    #+#             */
+/*   Updated: 2022/02/23 04:20:40 by semin            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
 
 int	main(int ac, char **av)
@@ -8,9 +20,18 @@ int	main(int ac, char **av)
 		return (0);
 	}
 	int	i = 1;
-	while (i < ac)
+	int	j;
+	while (av[i])
 	{
-		std::cout << av[i] << "";
+		j = 0;
+		while (av[i][j])
+		{
+			if (av[i][j] >= 'a' && av[i][j] <= 'z')
+				std::cout << (char)(av[i][j] - 32);
+			else
+				std::cout << av[i][j];
+			j++;
+		}
 		i++;
 	}
 	std::cout << "" << std::endl;
