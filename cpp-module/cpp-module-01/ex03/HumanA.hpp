65b267985ac399f6e391c6075158a6b67399e0d3
Sejeong Min <sj.min621@gmail.com>
2022-05-29 16:38:49 +0900

    cpp01-ex02,ex03

---
+++ cpp-module/cpp-module-01/ex03/HumanA.hpp
@@ -0,0 +1,19 @@
+#ifndef HUMANA_HPP
+# define HUMANA_HPP
+
+# include "Weapon.hpp"
+
+class HumanA {
+private:
+	Weapon		_weapon;
+	std::string	_name;
+
+public:
+	HumanA();
+	HumanA(Weapon weapon, std::string name);
+	~HumanA();
+
+	void	attack();
+};
+
+#endif
